b9c37dfe6f7200b187bf1740b5b0de409da27b78
Ady Levy <ady@homeis.com>
2018-10-21 11:41:19 +0300

    HOM-2386 indent

--- src/models/syncable.js
+++ src/models/syncable.js
@@ -201,7 +201,7 @@ class Syncable extends Root {
    * @private
    */
   _load() {
-      if (this.isDestroyed) return;
+    if (this.isDestroyed) return;
     this.syncState = SYNC_STATE.LOADING;
     this._xhr({
       method: 'GET',
